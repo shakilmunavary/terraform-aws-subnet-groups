99385f01352140b056c1e5a8a179c4d399ed7348
shakilmunavary <shakil.ahamed@gmail.com>
2019-03-28 14:33:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "this" {
   name        = "${var.name}"
   description = "Subnet group"
-  subnet_ids  = ["${var.subnet_ids"]
+  subnet_ids  = ["${var.subnet_ids}"]
 }
